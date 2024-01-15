c8bd9d4662cbf7092974288310a3050876e286eb
Wayne-Chou <110703587+Wayne-Chou@users.noreply.github.com>
2024-01-15 09:49:11 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/shopping/",
-<<<<<<< HEAD
+
 });
-=======
-})
->>>>>>> 88547670b595c6f01bf3bb398f2f2117ace63661
